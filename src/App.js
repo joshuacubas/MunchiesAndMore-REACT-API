a6eab06d7c2fb9e3cec1edf7f3f1465419427835
Joshua M Cubas <joshuacubas@yahoo.com>
2020-05-04 10:46:44 -0500

    added cond. rendering to app for logged in or not user

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 
 import RecipeContainer from './RecipeContainer/index.js'
 
-function App() {
-	console.log(process.env)
-  return (
-    <div>
-    	<div className="header-div">
-      		<h1>Munchies and More!</h1>
-      		<h3>A place to find and share your favorite recipes and tutorials</h3>
-      		<p>Need a nav bar with login and user info too</p>
-      	</div>
-      	<RecipeContainer />
-    </div>
-  );
+export default class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			loggedIn:false,
+			loggedInUsername: '' 
+		}
+	}
+	render() {
+		return(
+			<div>
+				<div className="header-div">
+					<h1>Munchies and More!</h1>
+					<h3>A place to find and share your favorite recipes and tutorials</h3>
+					<p>Need a nav bar with login and user info too</p>
+				</div>
+				{
+					this.state.loggedIn
+					?
+					<RecipeContainer />
+					:
+					<h3>Not logged in. Please Log in or Register.</h3>
+				}
+			</div>
+		);
+	}
 }
 
-export default App;
+
